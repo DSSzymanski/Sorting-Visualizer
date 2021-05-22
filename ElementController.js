59fc7fd19ff25d8582ce1e82b42fbe75c0afd9ca
dsszymanski <dan1993us@yahoo.com>
2021-05-21 21:45:52 -0400

    Documented existing code and added TODOs

--- ElementController.js
+++ ElementController.js
@@ -3,20 +3,43 @@ let init = () => {
 	initSlider();
 }
 
-
+/*
+ *Sets up svg window to preset size inside correct div.
+ *TODO: change to scale with window size.
+ */
 let initSvg = () => {
 	const svg = document.querySelector("#sortingDisplaySvg");
 	svg.setAttribute("width", "1000");
 	svg.setAttribute("height", "400");
 }
 
+/*
+ *Generates bar and text elements within svg.
+ *
+ *Uses svg window dimensions to create an array of bars and text representing
+ *	equally spaced bars to sort through.
+ *
+ *@param 	{int}	eleSize: int representing amount of numbers to sort through.
+ *TODOS: 2 below
+*/
 let generateSvgElements = (eleSize) => {
 	const svg = document.querySelector("#sortingDisplaySvg");
+
+	//remove all elements within svg to get blank svg
 	svg.querySelectorAll('*').forEach(element => element.remove());
+
+	//gets dimentions of svg in [width, height] format
 	svgDim = getDim(svg);
+
+	//constant to be used for the width of each bar
 	const width = svgDim[0]/eleSize;
+	
+	//constant used for sizing bars into equal heights based on svg window
+	//TODO: change for variable window size 
 	const height = 350/eleSize;
+
 	const values = [];
+	//TODO: below 2 for loops can be done in 1
 	for(j=1; j <= eleSize; j++) {
 		values.push(height*j);
 		text = createText(height*j, j, width);
@@ -28,24 +51,49 @@ let generateSvgElements = (eleSize) => {
 	}
 }
 
+/*
+ *Creates a text element within svg window.
+ *
+ *@param 	{int}		height: int representing the text value
+ *@param 	{int}		pos: int representing which bar the text belongs to and positioning
+ *@param 	{number}	width: decimal value represeting the width of the bar/text area.
+ *TODO: change positioning to window size from static value
+ */
 let createText = (height, pos, width) => {
+	//string for rotating text 90 degrees
 	const rotation = "rotate(90,"+(pos-.75)*width+","+350+")";
 	text = document.createElementNS("http://www.w3.org/2000/svg", 'text');
+	
+	//setup text element
 	text.setAttribute('x', (pos-.5)*width);
 	text.setAttribute('y', 350);
 	text.setAttribute('transform', rotation);
 	text.textContent = height;
+	
 	return text;
 }
 
+/*
+ *Creates a rect element representing a bar within svg window.
+ *
+ *@param 	{int}		height: int representing the text value
+ *@param 	{int}		pos: int representing which bar the text belongs to and positioning
+ *@param 	{number}	width: decimal value represeting the width of the bar/text area.
+ *TODO: change positioning to window size from static value
+ *TODO: change styling of bars
+ */
 let createRect = (height, pos, width) => {
-	style = "fill: white; stroke-width: 1; stroke: black"
+	//string for rect style
+	const style = "fill: white; stroke-width: 1; stroke: black"
 	rect = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
+
+	//setup rect
 	rect.setAttribute('x', pos*width);
 	rect.setAttribute('y', 350-height);
 	rect.setAttribute('width', width);
 	rect.setAttribute('height', height);
 	rect.setAttribute('style', style);
+	
 	return rect;
 }
 
