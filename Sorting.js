4cd9c701e5af8afdc32e9e68b0ddaf442f79c8d5
dsszymanski <dan1993us@yahoo.com>
2021-06-29 20:35:45 -0400

    Cleaned up insertion sort code by using double assignment instead of temp vars

--- Sorting.js
+++ Sorting.js
@@ -1,11 +1,10 @@
-//TODO: make into async function
 //TODO: document
 let insertionSort = async(rects, texts) => {
 	//i = represents position of current element
 	//j = represents which position i is being tested against
 	//key = int value of i
 	//temp = temporary to switch arr position of rects and texts
-	let i, key, j, temp;
+	let i, key, j;
 
 	for(i = 1; i < texts.length; i++) {
 		key = parseInt(texts[i].textContent);
@@ -14,13 +13,9 @@ let insertionSort = async(rects, texts) => {
 			//swaps translation of rectangles and texts
 			await swap(rects[j], rects[j+1], texts[j], texts[j+1]);
 			//swap array positioning within texts
-			temp = texts[j];
-			texts[j] = texts[j+1];
-			texts[j+1] = temp;
+			[texts[j], texts[j+1]] = [texts[j+1], texts[j]];
 			//swap array positioning withing rects
-			temp = rects[j];
-			rects[j] = rects[j+1];
-			rects[j+1] = temp;
+			[rects[j], rects[j+1]] = [rects[j+1], rects[j]];
 
 			j -= 1;
 		}
