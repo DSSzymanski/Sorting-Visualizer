eb8be6bf44b0311348a7e279da4355296224bcf2
dsszymanski <dan1993us@yahoo.com>
2021-07-01 21:16:45 -0400

    Documented all functions up to current

--- Sorting.js
+++ Sorting.js
@@ -1,4 +1,17 @@
-//TODO: document
+/**
+ * Insertion sort algorithm. Run when algorithm select box is on insersion sort and start button is clicked.
+ *
+ * Basic insertion sort using the text values found within the text svg elements to compare. Iterates through
+ * 	the texts array and compares the value against the previous value in the array. If the current value is
+ * 	less than the previous value, swap the two values (in both positioning of the svg element and the texts
+ *  and rects arrays). Continue this until the current value is greater than the element before it before
+ *  the iteration continues.
+ *
+ * @param	{array}		rects: array of rect svg elements in the svg window in order of being created (
+ * 							order of unsorted elements).
+ * @param	{array}		texts: array of text svg elements in the svg window in order of being created (
+ * 							order of unsorted elements).
+ */
 let insertionSort = async(rects, texts) => {
 	//currRectIdx = represents position of current element
 	//compRectIdx = represents which position currRectIdx is being tested against
@@ -21,6 +34,7 @@ let insertionSort = async(rects, texts) => {
 	}
 }
 
+//Testing fn.
 let print = (arr) => {
 	let string = "";
 	arr.forEach(element => string = string + element.textContent + " ");
