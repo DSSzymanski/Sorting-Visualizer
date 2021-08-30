0f3cb7d14d4c674282cc3e1b860ff30cfa86a92a
dsszymanski <dan1993us@yahoo.com>
2021-08-30 05:15:35 -0400

    Commented remaining empty functions and reviewed/modified exisiting comments/code.

--- ElementController.js
+++ ElementController.js
@@ -1,8 +1,12 @@
 var timeout = 1000; //used for intervals/timeouts
 
-const circleStyle =  "stroke-width: 1; fill: white;";
+const circleStyle =  "stroke-width: 1; fill: white;"; //css text for heap sort circles
 const MAX_HEAP_SIZE = 31; //max heap size that can fit in window
 
+/**
+ * init initializes the main SVG (and elements inside), the size slider, the speed slider,
+ * and the help icon hover text upon page load.
+ */
 let init = () => {
 	initSvg();
 	initSizeSlider();
@@ -21,14 +25,27 @@ let initHelpIcon = () => {
 
 /**
  *Sets up svg window to preset size inside correct div.
- *TODO: change to scale with window size.
  */
 let initSvg = () => {
 	const svg = document.querySelector("#sortingDisplaySvg");
 	svg.setAttribute("height", "400");
-	svg.setAttribute('width', '400')
+	svg.setAttribute('width', '400');
 }
 
+/**
+ * initHeapSVG initializes the elements of the inputed svg for the heap sort algorithm.
+ * Generates all the circles, lines, and text elements based on the inputed array
+ * of rect objects from the first svg's display.
+ * 
+ * @param 	{svg}		svg: inputted svg element to load the heap sort elements on to.
+ * @param 	{array}		rects: array of rect objects from the other svg display used to
+ * 							   generate the text elements and the quantity of the lines
+ * 							   and circles.
+ * 
+ * @returns	{array}		returns array filled with arrays of generated elements in the format
+ * 							[array of circles, array of lines, array of texts] to be used in
+ * 							heap sort algorithm.
+ */
 let initHeapSVG = (svg, rects) => {
 	let circles = [], texts = [];
 	[circles, texts] = initCirclesAndTexts(svg, rects);
@@ -36,29 +53,54 @@ let initHeapSVG = (svg, rects) => {
 	return [circles, lines, texts];
 }
 
+/**
+ * initCirclesandTexts initializes the circle and text elements used in the
+ * heap sort svg display. Positioning of the elements are calculated through
+ * the svg dimensions and the text values and quantities are based on the 
+ * inputed array of rect elements. Once elements are create, they are returned 
+ * in an array in the format [array of circles, array of texts].
+ * 
+ * NOTE: elements are generated based on a svg of a fixed size. Therefore, it can
+ * only generate for up to 31 input rects.
+ * 
+ * @param 	{svg}		svg: inputted svg element to load the heap sort elements on to.
+ * @param 	{array}		rects: array of rect objects from the other svg display used to
+ * 							   generate the text elements and the quantity of the lines
+ * 							   and circles.
+ * 
+ * @returns	{array}		returns array filled with arrays of generated elements in the format
+ * 							[array of circles, array of texts] to be used in
+ * 							heap sort algorithm.
+ */
 let initCirclesAndTexts = (svg, rects) => {
 	let circles = [], texts = [];
+	//maxRows, colPerRow used to limit amount of nodes generated to 31 
 	let maxRows = 5;
-	
-	let yPos = svg.getAttribute('height') / maxRows;
-	let yOffset = yPos / 2;
-
 	let colPerRow = 1;
-	let count = 0;
 	
+	//variable to stop algorithm if less than 31 rects are inputed
+	let count = 0;
+
+	let yPos = svg.getAttribute('height') / maxRows;//space allowed for each element on a row
+	let yOffset = yPos / 2;//offset to center elements
+
+	//iterate to create circle and text elements
 	for(let row = 0; row < maxRows; row++){
-		let xPos = svg.clientWidth / colPerRow;
-		let xOffset = xPos / 2;
+		let xPos = svg.clientWidth / colPerRow;//space allowed per row
+		let xOffset = xPos / 2;//offset to position
 		
 		for(let col = 0; col < colPerRow; col++){
+			//create circle element and add to svg
 			let newCircle = createCircle(col, row, xPos, yPos, xOffset, yOffset);
 			svg.appendChild(newCircle);
 			circles.push(newCircle);
 
+			//create text element and add to svg
 			let newText = createText(col, row, xPos, yPos, xOffset, yOffset, Math.floor(rects[count].getAttribute('height')));
 			svg.appendChild(newText);
 			texts.push(newText);
 
+			//check if all rects have been made
 			count++;
 			if(count == rects.length) {return [circles, texts];}
 		}
@@ -67,12 +109,33 @@ let initCirclesAndTexts = (svg, rects) => {
 	return [circles, texts];
 }
 
+/**
+ * Sets stroke of line connecting 2 circle elements to none
+ * to signifiy that the child element is removed from the heap.
+ * 
+ * @param 	{array}		lines: array of lines that is indexed to find the line to be removed.
+ * @param 	{int}		node: index of line to be removed.
+ */
 let deleteLine = (lines, node) => {
 	lines[node-1].setAttribute('stroke', 'none');
 }
 
+/**
+ * createLines creates line elements based on positioning of inputed array of circles.
+ * Starts at 2nd array and generates lines based on parent nodes and then adds to
+ * inputed svg array.
+ * 
+ * Used in heap sort.
+ * 
+ * @param 	{array} 	circles: array of circles that have already been generated.
+ * 								 Used to calculate line positioning.
+ * @param 	{svg}		svg: inputed svg for line elements to be added to.
+ * 
+ * @returns {array}		returns array of line elements generated.
+ */
 let createLines = (circles, svg) => {
 	let lines = []
+	//start from 2nd and create line based on it and it's parent node
 	for(let currNode = 1; currNode < circles.length; currNode++) {
 		let parentNode = getParentNode(currNode);
 		let newLine = createHeapLine(circles[parentNode], circles[currNode]);
@@ -82,6 +145,16 @@ let createLines = (circles, svg) => {
 	return lines;
 }
 
+/**
+ * createHeapLine generates singular line based on 2 inputed circles elements before returning the line.
+ * Parent node should be the first inputed circle and child node should be the second due to the line
+ * being positioned based on the radius length and direction.
+ * 
+ * @param 	{circle} 	circle1: circle representing parent node.
+ * @param 	{circle} 	circle2: circle representing child node.
+ * 
+ * @returns {line}		returns generated line element.
+ */
 let createHeapLine = (circle1, circle2) => {
 	let radius = parseInt(circle1.getAttribute('r')); //should be same for both circles
 
@@ -101,34 +174,59 @@ let createHeapLine = (circle1, circle2) => {
 	return line;
 }
 
+/**
+ * createText creates individual text element to be placed on svg.
+ * 
+ * @param 	{int}		col: integer representing which column the text element belongs to.
+ * @param 	{int}		row: integer representing which column the text element belongs to.
+ * @param 	{int}		xPos: integer representing the x positioning of the element.
+ * @param 	{int}		yPos: integer representing the y positioning of the element.
+ * @param 	{int}		xOffset: integer to center the element on the x axis.
+ * @param 	{int}		yOffset: integer to center the element on the y axis.
+ * @param 	{string} 	inputText: text to be used in text element.
+ * 
+ * @returns {text}		returns generated text element.
+ */
 let createText = (col, row, xPos, yPos, xOffset, yOffset, inputText) => {
 	let text = document.createElementNS("http://www.w3.org/2000/svg", "text");
 	text.setAttribute('x', (col * xPos) + xOffset);
 	text.setAttribute('y', (row * yPos) + 1.11 * yOffset);
+	//anchor used to center text in corresponding circle element
 	text.setAttribute('text-anchor', 'middle');
 	text.textContent = inputText;
 	
 	return text;
 }
 
+/**
+ * createCircle creates individual circle element to be placed on svg.
+ * 
+ * @param 	{int}		col: integer representing which column the circle element belongs to.
+ * @param 	{int}		row: integer representing which column the circle element belongs to.
+ * @param 	{int}		xPos: integer representing the x positioning of the element.
+ * @param 	{int}		yPos: integer representing the y positioning of the element.
+ * @param 	{int}		xOffset: integer to center the element on the x axis.
+ * @param 	{int}		yOffset: integer to center the element on the y axis.
+ * 
+ * @returns {text}		returns generated circle element.
+ */
 let createCircle = (col, row, xPos, yPos, xOffset, yOffset) => {
 	let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 	circle.setAttribute('cx', (col * xPos) + xOffset);
 	circle.setAttribute('cy', (row * yPos) + yOffset);
-	circle.setAttribute('r', 12);
+	circle.setAttribute('r', 12);//radius
 	circle.setAttribute('style', 'stroke: black;' + circleStyle);
 	
 	return circle;
 }
 
 /**
- * colorElement is called to set an inputed array of svg rects fill to inputed color.
+ * colorCircles sets color of 2 inputed circles.
  * 
- * @param 	{array}			rects: array of svg rect objects to be colored.
+ * @param 	{circle}		circle1: first circle element to be colored.
+ * @param 	{circle}		circle2: second circle element to be colored.
  * @param 	{string}		color: can be html color string (e.g. 'red') or a hex color code
- * 								   (e.g. '#FFFFFF').
- * 
- * @return 	{promise}		returns promise when completed to have outer function resume.
+ * 								   (e.g. '#FFFFFF'). 
  */
 let colorCircles = (circle1, circle2, color) => {
 	circle1.setAttribute('style', 'stroke: ' + color + ';' + circleStyle);
@@ -171,7 +269,7 @@ let startAlgorithm = async () => {
 	const INSERTION = "Insertion Sort";
 	const MERGE = "Merge Sort";
 	const QUICK = "Quick Sort";
-	const COMPARE_TRUE = 0;
+	const COMPARE_TRUE = 0;//basic true representation
 
 	//get select html element
 	const algSelect = document.querySelector("#algList");
@@ -196,6 +294,11 @@ let startAlgorithm = async () => {
 		bubbleSort(rects);
 	}
 	else if(HEAP.localeCompare(alg) == COMPARE_TRUE){
+		/*
+			check to make sure that the input to create the second svg is < the max heap size.
+			if it is, sets the elements to the max heap size and regenerates the svg display
+			before running.
+		*/
 		if(document.querySelector("#sizeSlider").value > MAX_HEAP_SIZE) {
 			document.querySelector("#sliderTB").innerText = "Size: " + MAX_HEAP_SIZE;
 			document.querySelector("#sizeSlider").value = MAX_HEAP_SIZE;
@@ -213,7 +316,7 @@ let startAlgorithm = async () => {
  *	equally spaced bars to sort through.
  *
  *@param 	{int}	eleSize: int representing amount of numbers to sort through.
- *TODOS: 1 below
+ * 							 Based on size slider.
 */
 let generateSvgElements = (eleSize) => {
 	const svg = document.querySelector("#sortingDisplaySvg");
@@ -228,7 +331,7 @@ let generateSvgElements = (eleSize) => {
 	const width = svgDim[0]/eleSize;
 	
 	//constant used for sizing bars into equal heights based on svg window
-	const height = 400/eleSize;
+	const height = svgDim[1]/eleSize;
 	
 	//randomize order of numbers used to generate rect/text values
 	let nums = [];
@@ -236,6 +339,7 @@ let generateSvgElements = (eleSize) => {
 	for(let i = 1; i <= eleSize; i++) {
 		nums.push(height*i);
 	}
+	//randomize the ordering of the numbers.
 	nums = nums.sort(() => Math.random() - 0.5);
 
 	for(let i = 1; i <= eleSize; i++) {
@@ -264,8 +368,8 @@ let removeLine = () => {
  * Function to add line separating left and right sides of arrays. Used only
  * 	in merge sort algorithm.
  * 
- * @param 	{string}			data: string representing translation of first rect of the right array.
- * 									Should always be in the format "translate({number})"
+ * @param 		{string}		data: string representing translation of first rect of the right array.
+ * 									  Should always be in the format "translate({number})".
  * 
  * @returns 	{svg line}		returns the generated line when finished.
  */
@@ -282,14 +386,14 @@ let createLine = (data) => {
 }
 
 /**
- * Returns rect svg element to the main svg window and moved to it's new position through translation.
+ * Returns rect svg element to the main svg window after it's moved to the second svg display.
+ * It's then moved to it's new position through translation. Used only in merge sort algorithm.
  * 
- * @param 	{svg rect}		rect: current svg rect object to be brought back on screen.
+ * @param 	{rect}			rect: current svg rect object to be brought back on screen.
  * @param 	{string}		translation: string representing the css translation used for the rect based on
  * 										 it's array position.
  * 
  * @return 	{promise}		returns promise when completed to have outer function resume.
- * 
  */
 let replaceElement = (rect, translation) => {
 	return new Promise((resolve) => {
@@ -309,9 +413,9 @@ let replaceElement = (rect, translation) => {
 /**
  * colorElement is called to set an inputed svg rect's fill to inputed color.
  * 
- * @param 	{svg rect}		rect: current svg rect object to be brought back on screen.
+ * @param 	{rect}			rect: current svg rect object to be colored.
  * @param 	{string}		color: can be html color string (e.g. 'red') or a hex color code
- * 								   (e.g. '#FFFFFF').
+ * 								   (e.g. '#FFFFFF'). Used to color rect.
  * 
  * @return 	{promise}		returns promise when completed to have outer function resume.
  */
@@ -356,7 +460,7 @@ let colorMultiEle = (rects, color) => {
  * @param 	{svg rect}	rect: rect object to be moved.
  */
 let hideElement = async(rect) => {
-	await colorElement(rect, 'red'); //color for moving off screen
+	await colorElement(rect, 'red'); //color to indicate moving to second screen
 	await moveToSecondSVG(rect); //move to second svg window
 }
 
@@ -379,7 +483,7 @@ let moveToSecondSVG = (rect) => {
 }
 
 /**
- * Searches Dom for all svg objects and returns the transformation strings in an array in order.
+ * Searches DOM for all svg objects and returns the transformation strings in an array in order.
  * 
  * @param 	{int}	length: amount of rects in svg.
  * 
@@ -410,7 +514,7 @@ let getSortingElements = () => {
  *@param 	{rect}		rect1: svg rectangle element to swap.
  *@param 	{rect}		rect2: svg rectangle element to swap.
  *
- *@returns	{promise}	returns promis when completed to move to next step.
+ *@returns	{promise}	returns promise when completed to move to next step.
  */
 let swap = (rect1, rect2) => {
 	return new Promise((resolve) => {
@@ -426,19 +530,25 @@ let swap = (rect1, rect2) => {
 
 /**
  * Creates second SVG window on screen. Used in merge sort to show sorted arrays to
- * 		be merged and in heap sort to visualize the heap.
+ * 		be merged and in heap sort to visualize the heap. Adds title banner in h3
+ * 		above second svg.
+ * 
+ * @param 	{string}	banner: string representing title to be added above second svg
+ * 								to indicate it's purpose.
+ * 
+ * @returns {svg} 		returns generated svg.
  */
 let createNewSVG = (banner) => {
 	//find location to be added
-	const svgRowDiv = document.getElementById('svgRow');
+	const svgRowDiv = document.getElementById('svgRow');//base element to attach to
+	//parent structure
 	const bootstrapCol = document.createElement('div');
 	bootstrapCol.setAttribute('class', 'col-lg-6 col-12');
+	
+	//text to be added
 	const bannerH3 = document.createElement('h3');
 	bannerH3.innerText = banner;
-	bootstrapCol.appendChild(bannerH3);
-
-	svgRowDiv.appendChild(bootstrapCol);
-
+	
 	//setup and add svg
 	let svg = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
 	svg.setAttribute("width", "402");
@@ -446,7 +556,11 @@ let createNewSVG = (banner) => {
 	svg.setAttribute("id", "secondSVG");
 	svg.setAttribute('viewbox', "0 0 400 400");
 	svg.setAttribute('preserveAspectRatio', 'none');
+	
+	//add everything to base div
 	bootstrapCol.appendChild(svg);
+	bootstrapCol.appendChild(bannerH3);
+	svgRowDiv.appendChild(bootstrapCol);
 
 	return svg;
 }
@@ -460,9 +574,7 @@ let createNewSVG = (banner) => {
  *@param 	{string}	idName: incremental string used for rect id.
  *
  *@returns	{SVG rect}	returns svg rectangle element for given inputs
- *TODO: change positioning to window size from static value
- *TODO: change styling of bars
- **/
+ */
 let createRect = (height, pos, width, idName) => {
 	//string for rect style
 	const style = "fill: white; stroke-width: 1; stroke: black";
@@ -482,27 +594,32 @@ let createRect = (height, pos, width, idName) => {
 }
 
 /**
- *Returns dimensions of svg element as an Integer number.
+ *Returns dimensions of svg element as an Integer number. Used to generate svg elements.
  *
- *@param {svg element}	svg 	svg parameter to get width and height dimensions from.
+ *@param {svg}		svg: svg parameter to get width and height dimensions from.
  *
- *@return {array}		arr	Returns 2 value array of [width, height] of inputed svg element.
+ *@return {array}	Returns 2 value array of [width, height] of inputed svg element.
  */
 let getDim = (svg) => {
 	return [svg.clientWidth, svg.clientHeight];
 }
 
+/**
+ * initSpeedSlider initializes the speed slider that controls the program's timeout value
+ * based on milliseconds. Also generates text above when the bar is slid.
+ * NOTE: bar can be functionally moved during algorithm.
+ */
 let initSpeedSlider = () => {
 	const slider = document.querySelector("#speedSlider");
 	let text = document.querySelector("#speedTB");
 
-	const initialValue = 500;
+	const initialValue = 500;//initial value of 1/2 second
 
-	//initialize slider, textbox, and svg elements with initial value
+	//initialize slider and textbox
 	slider.value = initialValue;
 	text.innerText = "Speed(ms): " + initialValue;
 
-	//setup slider to change text and svg elements when slid
+	//setup slider to change text and timeout value.
 	slider.oninput = function() {
 		text.innerText = "Speed(ms): " + this.value;
 		timeout = slider.value;
@@ -533,23 +650,3 @@ let initSizeSlider = () => {
 		generateSvgElements(this.value);
 	}
 }
-
-/**
- *Function used to increment the step counter text box by 1.
- */
-let incrementStep = () => {
-	const stepTB = document.querySelector("#stepTB");
-	stepTB.innerText = Number(stepTB.innerText) + 1;
-}
-/**
-let startTimer = () => {
-	const timeTB = document.querySelector("#timeTB");
-	let time = 0;
-	timerVar = setInterval(timer, 1000);
-
-	function timer() {
-		time = time+1;
-		timeTB.innerText = time;
-	}
-}
-**/
